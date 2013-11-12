8217aa3af77851d56a9b96289145549be082f5ab
bup <daniel.farkas0@gmail.com>
2013-11-12 12:05:58 +0100

    Mail account properties moved to constants class

--- src/main/org/bundolo/server/MailingUtils.java
+++ src/main/org/bundolo/server/MailingUtils.java
@@ -15,65 +15,61 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
+import org.bundolo.shared.Constants;
+
 public class MailingUtils {
-	
-	private static String from = "daniel.farkas0@gmail.com";
-	private static String smtpHost = "smtp.gmail.com";
-	private static String port = "465";
-	private static String login = "daniel.farkas0@gmail.com";
-	private static String password = "l0gibosus";
 
-	public static void main(String[] args) {
-		try {
-			sendEmail("guz", "vozdra1", "daniel_farkas@yahoo.com");
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MessagingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public static void main(String[] args) {
+	// just sending some test mail
+	try {
+	    sendEmail("guz", "vozdra1", "daniel_farkas@yahoo.com");
+	} catch (UnsupportedEncodingException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (MessagingException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+    }
 
-	public static void sendEmail(String body, String subject, String recipient)
-			throws MessagingException, UnsupportedEncodingException {
-		Properties mailProps = new Properties();
-		mailProps.put("mail.smtp.from", from);
-		mailProps.put("mail.smtp.host", smtpHost);
-		mailProps.put("mail.smtp.port", port);
-		mailProps.put("mail.smtp.auth", true);
-		mailProps.put("mail.smtp.socketFactory.port", port);
-		mailProps.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		mailProps.put("mail.smtp.socketFactory.fallback", "false");
-		mailProps.put("mail.smtp.starttls.enable", "true");
+    public static void sendEmail(String body, String subject, String recipient) throws MessagingException,
+	    UnsupportedEncodingException {
+	Properties mailProps = new Properties();
+	mailProps.put("mail.smtp.from", Constants.FROM);
+	mailProps.put("mail.smtp.host", Constants.SMTP_HOST);
+	mailProps.put("mail.smtp.port", Constants.PORT);
+	mailProps.put("mail.smtp.auth", true);
+	mailProps.put("mail.smtp.socketFactory.port", Constants.PORT);
+	mailProps.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+	mailProps.put("mail.smtp.socketFactory.fallback", "false");
+	mailProps.put("mail.smtp.starttls.enable", "true");
 
-		Session mailSession = Session.getDefaultInstance(mailProps,
-				new Authenticator() {
+	Session mailSession = Session.getDefaultInstance(mailProps, new Authenticator() {
 
-					@Override
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(login, password);
-					}
+	    @Override
+	    protected PasswordAuthentication getPasswordAuthentication() {
+		return new PasswordAuthentication(Constants.LOGIN, Constants.PASSWORD);
+	    }
 
-				});
+	});
 
-		MimeMessage message = new MimeMessage(mailSession);
-		message.setFrom(new InternetAddress(from));
-		String[] emails = { recipient };
-		InternetAddress dests[] = new InternetAddress[emails.length];
-		for (int i = 0; i < emails.length; i++) {
-			dests[i] = new InternetAddress(emails[i].trim().toLowerCase());
-		}
-		message.setRecipients(Message.RecipientType.TO, dests);
-		message.setSubject(subject, "UTF-8");
-		Multipart mp = new MimeMultipart();
-		MimeBodyPart mbp = new MimeBodyPart();
-		mbp.setContent(body, "text/html;charset=utf-8");
-		mp.addBodyPart(mbp);
-		message.setContent(mp);
-		message.setSentDate(new java.util.Date());
-
-		Transport.send(message);
+	MimeMessage message = new MimeMessage(mailSession);
+	message.setFrom(new InternetAddress(Constants.FROM));
+	String[] emails = { recipient };
+	InternetAddress dests[] = new InternetAddress[emails.length];
+	for (int i = 0; i < emails.length; i++) {
+	    dests[i] = new InternetAddress(emails[i].trim().toLowerCase());
 	}
+	message.setRecipients(Message.RecipientType.TO, dests);
+	message.setSubject(subject, "UTF-8");
+	Multipart mp = new MimeMultipart();
+	MimeBodyPart mbp = new MimeBodyPart();
+	mbp.setContent(body, "text/html;charset=utf-8");
+	mp.addBodyPart(mbp);
+	message.setContent(mp);
+	message.setSentDate(new java.util.Date());
+
+	Transport.send(message);
+    }
 
 }
