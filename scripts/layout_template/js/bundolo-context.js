12e91a7b487cf2fb7baf23bb00cb30dcdf3e9d3d
daniel <daniel@192.168.1.63>
2014-04-15 22:37:29 +0200

    Comments button

--- scripts/layout_template/js/bundolo-context.js
+++ scripts/layout_template/js/bundolo-context.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
-	$('.context-button').click(function(e) {
+	var item = $('<span class="fa-stack fa-2x comments-button">\
+			<i class="fa fa-circle fa-stack-2x"></i>\
+			<i class="fa fa-comment-o fa-stack-1x fa-inverse"></i>\
+			</span>');
+	item.click(function(e) {
 		setContextMenuPostion(e, $('.context-menu'));				        
         return false;
     });
+	$('.content').append(item);
     $(document).click(function(event) { 
         if($(event.target).parents().index($('.context-menu')) == -1) {
             if($('.context-menu').is(":visible")) {
@@ -10,6 +15,19 @@ $(document).ready(function() {
             }
         }        
     })
+    $('.comments-button').hover(
+    	function() {
+    		$(this).parent().addClass("hover");
+    		if ($(this).parent().parent().css("overflow")=="hidden") {
+    			$(this).parent().parent().addClass("show-overflow");
+    		}
+    		//alert("in: "+$(this).parent().attr("class"));
+    	}, function() {
+    		$(this).parent().removeClass("hover");
+    		$(this).parent().parent().removeClass("show-overflow");
+    		//alert("out: "+$(this).parent().attr("class"));
+    	}
+    );
 });
 
 function setContextMenuPostion(event, contextMenu) {
--- scripts/layout_template/js/bundolo.js
+++ scripts/layout_template/js/bundolo.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+  $('.content').attr("title", "Comments");
   $('[title]').hover(
     function() {
 		$('.status_bar>div').text($(this).attr('title'));
@@ -12,7 +13,7 @@ $(document).ready(function() {
 });
 
 function displayDummyText() {
-	$(".main").html('<div class="jumbotron">\
+	$(".main").html('<div class="jumbotron content">\
 	<h1>text title</h1>\
 		<p>Combtooth blenny houndshark clown triggerfish paperbone,\
 			"European eel tilapia sea snail tilapia waryfish," Bitterling\
@@ -38,12 +39,6 @@ function displayDummyText() {
 			Billfish gray reef shark Razorback sucker flounder quillback;\
 			clownfish medusafish Atlantic trout? Gouramie bichir frilled shark\
 			dragonet spiny dogfish cuckoo wrasse.</p>\
-		<button type="button" class="btn btn-default context-button" title="Comments">\
-			<span class="fa-stack">\
-				<i class="fa fa-circle fa-stack-2x"></i>\
-				<i class="fa fa-info fa-stack-1x fa-inverse"></i>\
-			</span>\
-		</button>\
 	</div>');
 	
 }
