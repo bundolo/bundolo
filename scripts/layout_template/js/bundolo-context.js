dcd61f9a2dcc168f75a92eea0d3963eaa32326d9
daniel <daniel@192.168.1.63>
2014-04-18 21:13:34 +0200

    Comments context menu

--- scripts/layout_template/js/bundolo-context.js
+++ scripts/layout_template/js/bundolo-context.js
@@ -1,39 +1,8 @@
+var accordionCounter = 0;
+var commentCounter = 0;
+var dummyCommentCounter = 0;
 $(document).ready(function() {
-	var item = $('<span class="fa-stack fa-2x comments-button">\
-			<i class="fa fa-circle fa-stack-2x"></i>\
-			<i class="fa fa-comment-o fa-stack-1x fa-inverse"></i>\
-			</span>');
-	item.click(function(e) {
-		setContextMenuPostion(e, $('.context-menu'));				        
-        return false;
-    });
-	$('.content').append(item);
-    $(document).click(function(event) { 
-        if($(event.target).parents().index($('.context-menu')) == -1) {
-            if($('.context-menu').is(":visible")) {
-                $('.context-menu').hide()
-            }
-        }        
-    })
-    $('.comments-button').attr("title", "Comments");
-	$('[title]').hover(
-		function() {
-			$('.status_bar>div').text($(this).attr('title'));
-		}, function() {
-			$('.status_bar>div').text('');
-		}
-	);
-    $('.comments-button').hover(
-    	function() {
-    		$(this).parent().addClass("hover");
-    		if ($(this).parent().parent().css("overflow")=="hidden") {
-    			$(this).parent().parent().addClass("show-overflow");
-    		}
-    	}, function() {
-    		$(this).parent().removeClass("hover");
-    		$(this).parent().parent().removeClass("show-overflow");
-    	}
-    );
+
 });
 
 function setContextMenuPostion(event, contextMenu) {
@@ -64,4 +33,86 @@ function setContextMenuPostion(event, contextMenu) {
         top: menuPostion.y
     });
     return menuPostion;
+}
+
+function addContextMenu(parentElement) {
+	var contextMenu =  $('<div class="context-menu panel-group" id="accordion' + accordionCounter++ + '"><div><div></div></div></div>');
+	//close context menu if it is open and there has been a click outside of it
+	$(document).click(function(event) { 
+        if($(event.target).parents().index(contextMenu) == -1) {
+            if(contextMenu.is(":visible")) {
+            	contextMenu.hide()
+            }
+        }        
+    })
+	var commentsRoot = contextMenu.find("div>div");
+	var rootCommentButton = addCommentButton(commentsRoot, commentsRoot);
+	rootCommentButton.click(function(e) {
+		addComment(commentsRoot, 'comment'+commentCounter++);
+        return false;
+    });
+	$('body').append(contextMenu);
+	var commentsButton = $('<span class="fa-stack fa-2x comments-button">\
+			<i class="fa fa-circle fa-stack-2x"></i>\
+			<i class="fa fa-comment-o fa-stack-1x fa-inverse"></i>\
+			</span>');
+	commentsButton.click(function(e) {
+		setContextMenuPostion(e, contextMenu);				        
+        return false;
+    });
+	commentsButton.attr("title", "Comments");
+	commentsButton.hover(
+		function() {
+			$('.status_bar>div').text($(this).attr('title'));
+			$(this).parent().addClass("hover");
+    		if ($(this).parent().parent().css("overflow")=="hidden") {
+    			$(this).parent().parent().addClass("show-overflow");
+    		}
+		}, function() {
+			$('.status_bar>div').text('');
+			$(this).parent().removeClass("hover");
+    		$(this).parent().parent().removeClass("show-overflow");
+		}
+	);
+	parentElement.append(commentsButton);
+	
+}
+
+function addComment(parentElement, commentText) {
+	var comment = $('<div class="panel panel-default comment" id="comment'+commentCounter+'"></div>');
+	var commentContent = $('<div class="panel-heading"><span>'+commentText+'</span></div><div id="collapse1" class="panel-collapse"><div class="panel-body"><div class="panel-group" id="accordion1"><div><div></div></div></div></div></div>');
+	var commentCommentButton = addCommentButton(comment);
+	comment.append(commentContent);
+	parentElement.append(comment);
+	commentCommentButton.click(function(e) {
+		addComment(comment.find(">.panel-collapse>div>div>div>div"), 'comment'+commentCounter+++" "+Math.random() + " "+Math.random());
+        return false;
+    });
+	if(dummyCommentCounter-- >0) {
+	//TODO remove this after testing is done
+		commentCommentButton.click();
+	}
+}
+
+function addCommentButton(parentElement) {
+	var commentButton = $('<span title="Add comment" class="fa-stack fa-2x pull-right comment-button" id="comment-button'+commentCounter+'">\
+			<i class="fa fa-circle fa-stack-2x"></i>\
+			<i class="fa fa-plus fa-stack-1x fa-inverse"></i>\
+			</span>');
+
+	commentButton.hover(
+			function() {
+				$('.status_bar>div').text($(this).attr('title'));
+				$(this).parent().addClass("hover");
+	    		if ($(this).parent().parent().css("overflow")=="hidden") {
+	    			$(this).parent().parent().addClass("show-overflow");
+	    		}
+			}, function() {
+				$('.status_bar>div').text('');
+				$(this).parent().removeClass("hover");
+	    		$(this).parent().parent().removeClass("show-overflow");
+			}
+		);
+	parentElement.append(commentButton);
+	return commentButton;
 }
--- scripts/layout_template/js/bundolo.js
+++ scripts/layout_template/js/bundolo.js
@@ -1,12 +1,30 @@
+var homeHtml = "";
 $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
-  });  
+  });
+  $('[title]').hover(
+	function() {
+		$('.status_bar>div').text($(this).attr('title'));
+	}, function() {
+		$('.status_bar>div').text('');
+	}
+  );
+  var mainContent = $(".main>.jumbotron>.content");
+  homeHtml = mainContent.html();
+  addContextMenu(mainContent);
 });
 
+function displayContent(parentElement, html) {
+	parentElement.html(html);
+	addContextMenu(parentElement);
+}
+
+function displayHomeText() {
+	displayContent($(".main>.jumbotron>.content"), homeHtml);
+}
 function displayDummyText() {
-	$(".main").html('<div class="jumbotron content">\
-	<h1>text title</h1>\
+	displayContent($(".main>.jumbotron>.content"), '<h1>text title</h1>\
 		<p>Combtooth blenny houndshark clown triggerfish paperbone,\
 			"European eel tilapia sea snail tilapia waryfish," Bitterling\
 			crocodile shark. Flagblenny Hammerjaw stonecat freshwater herring\
@@ -30,7 +48,5 @@ function displayDummyText() {
 			weeverfish Rainbowfish leaffish. Rudderfish alligatorfish,\
 			Billfish gray reef shark Razorback sucker flounder quillback;\
 			clownfish medusafish Atlantic trout? Gouramie bichir frilled shark\
-			dragonet spiny dogfish cuckoo wrasse.</p>\
-	</div>');
-	
+			dragonet spiny dogfish cuckoo wrasse.</p>');
 }
